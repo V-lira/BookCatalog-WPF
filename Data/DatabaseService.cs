7aacaef96d8b39ff45bb3695926025cb9957260e
V-lira <v42719501@gmail.com>
2026-05-05 19:10:58 +0500

    Add files via upload

---
+++ Data/DatabaseService.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+
+namespace BookCatalogApp.Data
+{
+    public class DatabaseService
+    {
+        private string connectionString = "Data Source=books.db";
+
+        public DatabaseService()
+        {
+            InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+            CREATE TABLE IF NOT EXISTS Books (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Title TEXT NOT NULL,
+                Author TEXT NOT NULL,
+                Year INTEGER
+            );
+            ";
+
+            command.ExecuteNonQuery();
+        }
+        public void AddBook(Book book)
+        {
+            var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+            INSERT INTO Books (Title, Author, Year)
+            VALUES ($title, $author, $year);
+            ";
+
+            command.Parameters.AddWithValue("$title", book.Title);
+            command.Parameters.AddWithValue("$author", book.Author);
+            command.Parameters.AddWithValue("$year", book.Year);
+
+            command.ExecuteNonQuery();
+        }
+        public void DeleteBook(int id)
+        {
+            var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = "DELETE FROM Books WHERE Id = $id;";
+            command.Parameters.AddWithValue("$id", id);
+
+            command.ExecuteNonQuery();
+        }
+        public List<Book> GetAllBooks()
+        {
+            var books = new List<Book>();
+
+            var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Books;";
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                books.Add(new Book
+                {
+                    Id = reader.GetInt32(0),
+                    Title = reader.GetString(1),
+                    Author = reader.GetString(2),
+                    Year = reader.GetInt32(3)
+                });
+            }
+
+            return books;
+        }
+        public List<Book> SearchBooks(string title)
+        {
+            var books = new List<Book>();
+
+            var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            "SELECT * FROM Books WHERE Title LIKE $title;";
+            command.Parameters.AddWithValue("$title", "%" + title + "%");
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                books.Add(new Book
+                {
+                    Id = reader.GetInt32(0),
+                    Title = reader.GetString(1),
+                    Author = reader.GetString(2),
+                    Year = reader.GetInt32(3)
+                });
+            }
+
+            return books;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BookCatalogApp.Data;
+
+namespace BookCatalogApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DatabaseService db = new DatabaseService();
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadBooks();
+        }
+        private void LoadBooks()
+        {
+            BooksGrid.ItemsSource = db.GetAllBooks();
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            int year;
+
+            if (!int.TryParse(YearBox.Text, out year))
+            {
+                MessageBox.Show("Год должен быть числом!");
+                return;
+            }
+
+            var book = new Book
+            {
+                Title = TitleBox.Text,
+                Author = AuthorBox.Text,
+                Year = year
+            };
+
+            db.AddBook(book);
+            LoadBooks();
+        }
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (BooksGrid.SelectedItem is Book selectedBook)
+            {
+                db.DeleteBook(selectedBook.Id);
+                LoadBooks();
+            }
+            else
+            {
+                MessageBox.Show("Выбери книгу!");
+            }
+        }
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            BooksGrid.ItemsSource = db.SearchBooks(TitleBox.Text);
+        }
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            LoadBooks();
+        }
+    }
+}
